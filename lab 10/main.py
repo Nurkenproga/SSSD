9aca2f75ab3527a4d9d22625c2e301a1d9a60c07
Nurkenproga <n_atabay@kbru.com>
2025-11-18 23:17:15 +0500

    lab 10 finish

--- lab 10/main.py
+++ lab 10/main.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import requests
 
 def run_app():
     secret_env = os.getenv("API_KEY")
@@ -8,6 +9,17 @@ def run_app():
     with open("config.yaml") as f:
         data = yaml.safe_load(f)
         print("Config file secret:", data["db_password"])
+
+    VAULT_ADDR = "http://127.0.0.1:8200"
+    VAULT_TOKEN = os.getenv("VAULT_TOKEN")
+
+    url = f"{VAULT_ADDR}/v1/secret/data/app"
+    headers = {"X-Vault-Token": VAULT_TOKEN}
+
+    resp = requests.get(url, headers=headers).json()
+    vault_pass = resp["data"]["data"]["password"]
+
+    print("Vault secret:", vault_pass)
 
 if __name__ == "__main__":
     run_app()
